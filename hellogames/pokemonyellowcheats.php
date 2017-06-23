935921ff266edff19c79aeb7713c3cdc7ec5b76e
Ju Hong Kim <nabimoon1234@gmail.com>
2017-06-23 19:33:59 -0400

    Update pokemonyellowcheats.php

--- hellogames/pokemonyellowcheats.php
+++ hellogames/pokemonyellowcheats.php
@@ -36,7 +36,65 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/template/prepare_sql.php";
     		<div class="panel panel-primary">
       			<div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#collapseY"><a class="accordion-toggle"><h4 class="panel-title">Pokemon Yellow</a></h4></div>
     			<div id="collapseY" class="panel-collapse collapse in">  
-    				<div class="panel-body">test
+    				<div class="panel-body">
+					<div class="panel-body">
+	 			<?php
+  
+				$sql = "SELECT * FROM cheats WHERE game_id = :game_id";
+				$items = array_prepare_select ($sql, $pdo, ['game_id' => 3]);				
+				
+				foreach ($items as $item) {
+					echo "<div id='br'>";
+					echo "<p data-toggle='collapse' data-target='#collapse$item[cheat_id]'><b><a class = 'toggle'>$item[title]</a></b></p>";
+					echo "<div id = 'collapse$item[cheat_id]' class='collapse'>$item[content]</div>";
+					echo "</div>";
+					echo "<p>";
+				}
+				?>
+      				</div>
+      			</div>
+    		</div>
+			
+		<div class="panel panel-primary">
+      			<div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#collapseY"><a class="accordion-toggle"><h4 class="panel-title">Pokemon Blue</a></h4></div>
+    			<div id="collapseY" class="panel-collapse collapse in">  
+    				<div class="panel-body">
+					<div class="panel-body">
+	 			<?php
+  
+				$sql = "SELECT * FROM cheats WHERE game_id = :game_id";
+				$items = array_prepare_select ($sql, $pdo, ['game_id' => 2]);				
+				
+				foreach ($items as $item) {
+					echo "<div id='br'>";
+					echo "<p data-toggle='collapse' data-target='#collapse$item[cheat_id]'><b><a class = 'toggle'>$item[title]</a></b></p>";
+					echo "<div id = 'collapse$item[cheat_id]' class='collapse'>$item[content]</div>";
+					echo "</div>";
+					echo "<p>";
+				}
+				?>
+      				</div>
+      			</div>
+    		</div>	
+			
+		<div class="panel panel-primary">
+      			<div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#collapseY"><a class="accordion-toggle"><h4 class="panel-title">Pokemon Red</a></h4></div>
+    			<div id="collapseY" class="panel-collapse collapse in">  
+    				<div class="panel-body">
+					<div class="panel-body">
+	 			<?php
+  
+				$sql = "SELECT * FROM cheats WHERE game_id = :game_id";
+				$items = array_prepare_select ($sql, $pdo, ['game_id' => 1]);				
+				
+				foreach ($items as $item) {
+					echo "<div id='br'>";
+					echo "<p data-toggle='collapse' data-target='#collapse$item[cheat_id]'><b><a class = 'toggle'>$item[title]</a></b></p>";
+					echo "<div id = 'collapse$item[cheat_id]' class='collapse'>$item[content]</div>";
+					echo "</div>";
+					echo "<p>";
+				}
+				?>
       				</div>
       			</div>
     		</div>
