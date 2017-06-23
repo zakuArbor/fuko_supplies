f42e571fa43ca5b2eed0916b10e0ed21700a337f
zakuArbor <nabimoon1234@gmail.com>
2017-06-23 18:16:46 -0400

    Basic layout for index and game cheats completed

--- hellogames/index.php
+++ hellogames/index.php
@@ -1,67 +1,27 @@
+<?php $active = 0; ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+<?php include 'head.php' ?>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="/hellogames/">Home</a></li>
-        
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="/hellogames/gamecheats.php">Game Cheats <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="/hellogames/pokemonyellowcheats.php">Pokemon R/B/Y</a></li>
-            <li><a href="/hellogames/pokemonsilvercheats.php">Pokemon S/G</a></li>
-          </ul>
-        </li>
-
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Video <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="/hellogames/pokemonyellowvideo.php">Pokemon R/B/Y</a></li>
-            <li><a href="/hellogames/pokemonsilvervideo.php">Pokemon S/G</a></li>
-          </ul>
-        </li>
-
-        <li><a href="/hellogames/credit.php">Contact</a></li>
-        <li><a href="#">Credits</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php include 'nav.php' ?>
   
 <div class="container">
-  <h3>Welcome</h3>
-  Welcome to my site. It has been a while since I have done a major update to my site. The site layout has changed signficantly now supporting mobile devices, firefox, ie10 (hovereffect problem), and safari. I have quit studying web designing since entering highschool, so this probably the last major change I'll have in the site.
-<p>2013-10-29: I decided to play around with PHP and the site is expected to be updated significantly as I study more about PHP and programming. I thinking of re-learning web designing and go further into programming and web designing until I graduate from High School</p>
-<p>2013-11-30: Hellogames will no longer just be a site that serves my web development and game cheats, but will now have some basic programming lessons. Please note that the programming lessons are not 100% accurately but it is good to consider</p>
-<p>2016-06-19: This site has been very inactive due to my laziness. Hopefully, there will be updates done to the site.</p>
-<p><a href = "http://hellogames.net63.net/hellogames/PHP_Testing/">Some PHP TEST</a></p>
+  <h2>Welcome</h2>
+
+    <p><b>2017-06-23:</b> Welcome to my site. I quickly redesign the website to look more modern and simple that has mobile-first design approach. Since this website was founded back when I was in middle school, not much has changed since I have been very inactive over these past 6-8 years (Man I feel old). This website is just mainly a webpage that contains old Gameboy Color Games tips.</p>
+
+    <blockquote>Welcome to my site. It has been a while since I have done a major update to my site. The site layout has changed signficantly now supporting mobile devices, firefox, ie10 (hovereffect problem), and safari. I have quit studying web designing since entering highschool, so this probably the last major change I'll have in the site.</blockquote>
+<p><b>2017-06-23:</b> That surely was not the case. The previous website was not a mobile website but just reffited. Thus today's website layout release is a mobile-first design concept using Bootstrap. (I know I have said I would not make any major changes in the website). As an update, I have actually studied a bit on web programming in Highschool by focusing on the serverside programming such as PHP and databases. In addition, I have played around with FreeBootCamp recently, a free open source community that has lots of great coding challenges to help people learn webdesign. I recommend people to try it out.</p>  
+<blockquote><p><b>2013-10-29:</b> I decided to play around with PHP and the site is expected to be updated significantly as I study more about PHP and programming. I thinking of re-learning web designing and go further into programming and web designing until I graduate from High School</p></blockquote>
+<p><b>2017-06-23:</b>As I said earlier, I did study programming further in Highschool for fun. However, I was hoping to not study programming after high school but that never happened. Unfortunately (or fortunately), I am in university studying Computer Science at some Canadian University even though I never applied for the program. I will be further studying databases in the Fall and Web Computing in the Winter since they are offered in my university. However, I probably will not aim for a Computer Science job even though I am studying Computer Science because I have not much interest in Computer Science other than Operating Systems and Security. </p>
+---------------------------------------------------------
+<p><a href = "https://github.com/zakuArbor/"></a>My GITHUB Account</p>
+<p><a href = "/hellogames/PHP_Testing/">Some PHP TEST</a></p>
 <p><a href = "/hellogames/pdf/Arrays.pdf"> Arrays -IN PDF FILE</a></p>
 <p>Developing a beta site for my possible summative assignment: <a href = "/hellogames/test1/index.html">Fuko's Supplies</a></p>
-
+<p></p>
 </div>
-
+<?php include 'footer.php'; ?>
 </body>
 </html>
---
+++ hellogames/pokemonyellowcheats.php
@@ -0,0 +1,98 @@
+<?php $active = 0; ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php' ?>
+<body>
+
+<?php include 'nav.php' ?>
+  
+<div class="container">
+  <h3><u>Pokemon Yellow/Red/Blue/Green</u></h3>
+  <div class="panel-group" id="accordion">
+    <div class="panel panel-primary">
+      <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#collapseRBY"><a class="accordion-toggle"><h4 class="panel-title">Pokemon R/B/Y</a></h4></div>
+      <div id="collapseRBY" class="panel-collapse collapse">
+    
+      <div class="panel-body">
+        
+	<div id="br">
+	<b>How to get Squirtle in Pokemon Yellow</b><p>
+	To get Squirtle you have to beat Lt.Surge. Lt Surge is the third gym leader. Then go talk to officer Jenny and you get Squirtle lv.10. This cheat only works on Pokemon Yellow.
+	</div>
+	<p>
+	<div id="br">
+	<b>How to get Balbasaur in Pokemon Yellow</b><p>
+	In order to Bulbasaur, you go to Cerulean City and defeat Misty, the 2nd gym leadder. Then defeat the five trainers in the nugget bridge and go to Bill's house. Then go defeat Team Rocket and get the tm dig from him. Then go left and you will see a long house, and inside you will a oddish and a sandshrew. Go talk to the woman in the house and you will get Balbasaur. This cheat only works for Pokemon Yellow. 
+	</div>
+	<p>
+	<div id="br">
+	<b>How to get Charmander in Pokemon Yellow</b><p>
+	To get Charmander, go to Cerulean City and defeat the nugget bridge. There should be 5 trainers. Then go talk to a guy who is really close to an item I think the item thunder wave and he will give you Charmander. This cheat only works in Pokemon Yellow.
+         </div>
+      </div>
+    </div>
+    </div>
+    <div class="panel panel-primary">
+      <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#collapseY"><a class="accordion-toggle"><h4 class="panel-title">Pokemon Yellow</a></h4></div>
+    <div id="collapseY" class="panel-collapse collapse in">  
+    <div class="panel-body">Panel Content
+	<div id="br"><p>
+	<b>How to get the tm Mimic</b><p>
+	Go buy a pokedoll in Celedon Mart, then go to Saffron City. Then give the doll to Copycat.
+	</div>
+	<p>
+	<div id="br"><p>
+	<b>How to get Mewtwo</b><p>
+	Defeat the Elite Four then go to Cerulean City and go to the horror cave. Inside the horror cave there should be strong pokemon. Go explore the cave and then you will meet Mewtwo.
+	</div>
+	<p>
+	<div id="br"><p>
+	<b>What Pokemon cannot you not get in Pokemon Yellow</b><p>
+	<u>You cannot get:</u>
+	<p>Abrok<p>Beedrill<p>Ekans<p>Electabuzz<p>Jynx<p>Kakuna<p>Koffing<p>Magmar<p>Meowth<p>Persian<p>Raichu<p>Weedle<p>Weezing 
+	</div>
+	<p>
+	<div id="br"><p>
+	<b>What Pokemon cannot you not get in Pokemon Red</b><p>
+	<u>You cannot get:</u>
+	<p>Bellsprout<p>Magmar<p>Meowth<p>Ninetales<p>Persain<p>Prisir<p>Sandshrew<p>Sandslash<p>Victreebel<p>Vulpix<p>Weepinbell
+	</div>
+	<p>
+	<p><div id="br"><p>
+	What Pokemon cannot you not get in Pokemon Blue</b><p>
+	<u>You cannot get:</u> 
+	<p>Arbok<p>Arcanine<p>Enkans<p>Electabuzz<p>Gloom<p>Growlile<p>Mankey<p>Oddish<p>Primeape<p>Scyther<p>Vileplume
+	</div>
+	<p>
+	<p><div id="br"><p>
+	<b>Having a hard time going through the dark cave?</b><p>
+	Go to route 2 and you will find one of Proff.Oak aide and he will give you the hm flash. 
+	</div>
+	<p><div id="br"><p>
+	<b>How to Catch Evee</b><p>
+	To get Evee, just go to to Celadon City. Then go behind the pokecenter and enter a big building. The big building is called Celadon Mansion. There are two doors to get in and out. One is located near the post that says "Celadon Mansion". The other one is behind the mansion. Enter the back door, this is why we go behind the Pokemon Center. Go to the top floor and enter the room. Then you will see a man and a table right beside him. Go to the table and you will see a pokeball. Click on the pokeball and you will get Evee.
+	<p>
+	</div>
+	<p><div id="br"><p>
+	<b>How to get pass the Ghost Id in Poke Tower</b><p>
+	In the Poke Tower, there will be a ghost id blocking the stairs. To defeat Ghost Id there are two ways. Go defeat Team Rocket hideout in Celadon City inside the Game Corner and get Silph Scope, which you have to in order to battle the leaf gym.
+	Or you can buy a Pokedoll and use it when battling Ghost Id.
+	</div>
+	<p><div id="br"><p>
+	<b>How to wake up Snorelax</b><p>
+	In route 12 and 16, Snorelax is blocking the path. To wake him up, you need to defeat Cubone mother in the Ghost Tower(Pokemon Tower).Then defeat Team Rocket, then you will meet Mr. Fiji and he will give the Poke Flute. This is a useful item to wake up Snorelax and for your Pokemon when they are asleep in battle.
+	When you wake Snorelax up, you have to battle him. You can only see Snorelax twice in the game. One in route 12 and the other in route 16.
+	</div>
+	<p><div id="br"><p>
+	<b>How to go inside Saffron City</b><p>
+	To go inside Saffron City, go buy a drink from the Vending machine in Celadon Dept Store. Go to one of the Saffron Gates and give a drink to the guard and he will let you go inside the city.
+	<p>
+	</div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
+</body>
+</html>
