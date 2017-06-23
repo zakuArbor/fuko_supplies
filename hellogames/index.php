3a809d1a75f4763901c273af47c6131b49eee6b0
zakuArbor <nabimoon1234@gmail.com>
2017-06-23 14:57:28 -0400

    Added first mobile page

---
+++ hellogames/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="/hellogames/">Home</a></li>
+        
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="/hellogames/gamecheats.php">Game Cheats <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/hellogames/pokemonyellowcheats.php">Pokemon R/B/Y</a></li>
+            <li><a href="/hellogames/pokemonsilvercheats.php">Pokemon S/G</a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Video <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/hellogames/pokemonyellowvideo.php">Pokemon R/B/Y</a></li>
+            <li><a href="/hellogames/pokemonsilvervideo.php">Pokemon S/G</a></li>
+          </ul>
+        </li>
+
+        <li><a href="/hellogames/credit.php">Contact</a></li>
+        <li><a href="#">Credits</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Welcome</h3>
+  Welcome to my site. It has been a while since I have done a major update to my site. The site layout has changed signficantly now supporting mobile devices, firefox, ie10 (hovereffect problem), and safari. I have quit studying web designing since entering highschool, so this probably the last major change I'll have in the site.
+<p>2013-10-29: I decided to play around with PHP and the site is expected to be updated significantly as I study more about PHP and programming. I thinking of re-learning web designing and go further into programming and web designing until I graduate from High School</p>
+<p>2013-11-30: Hellogames will no longer just be a site that serves my web development and game cheats, but will now have some basic programming lessons. Please note that the programming lessons are not 100% accurately but it is good to consider</p>
+<p>2016-06-19: This site has been very inactive due to my laziness. Hopefully, there will be updates done to the site.</p>
+<p><a href = "http://hellogames.net63.net/hellogames/PHP_Testing/">Some PHP TEST</a></p>
+<p><a href = "/hellogames/pdf/Arrays.pdf"> Arrays -IN PDF FILE</a></p>
+<p>Developing a beta site for my possible summative assignment: <a href = "/hellogames/test1/index.html">Fuko's Supplies</a></p>
+
+</div>
+
+</body>
+</html>
